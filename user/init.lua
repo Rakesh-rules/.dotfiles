bde09b47ad73a38d588e6d4352cfc03617584b42
Rakesh-rules <rakeshfire97@gmail.com>
2022-04-13 08:58:11 +0530

    zsh vi mode + html snippets in JS(x)/TS(x)

--- user/init.lua
+++ user/init.lua
@@ -1,1 +1,5 @@
 require('telescope').setup{  defaults = { file_ignore_patterns = { "node_modules" }} }
+require("luasnip").filetype_extend("javascript", { "html" })
+require("luasnip").filetype_extend("javascriptreact", { "html" })
+require("luasnip").filetype_extend("typescript", { "html" })
+
