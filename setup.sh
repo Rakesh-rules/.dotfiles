10f15045719d2876048de7835b82a34ce8fd4587
Rakesh-rules <rakeshfire97@gmail.com>
2022-04-18 08:23:06 +0530

    formatting (prettier,autopep8) + cuda install + conda and pytorch install

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo add-apt-repository ppa:lazygit-team/release
 sudo apt update && sudo apt upgrade
-sudo apt install build-essential neovim neofetch unzip zip fzf lazygit python3-neovim
+sudo apt install build-essential neovim neofetch unzip zip fzf lazygit python3-neovim python3-pip
 
 ### zsh setup
 
@@ -17,6 +17,30 @@ source ~/.zshrc; clear
 }	
 zsh_source
 
+### CUDA 
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-4-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+rm cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
+
+### Conda and Pytorch install
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh 
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+zsh_source
+conda config --set auto_activate_base false
+zsh_source
+conda create --name torch python=3.8 -y
+conda activate torch
+conda install ipykernel
+conda install nb_conda # jupyter support to new environment
+conda install pytorch cudatoolkit -c pytorch # pytorch install
+python -m ipykernel install --user --name pytorch --display-name "Python 3.7 (pytorch)"
+
 ### Node Installation
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | zsh 
@@ -26,12 +50,16 @@ nvm use node
 node -v
 zsh_source
 
+## linter and formatter
+npm install -g prettier
+pip install --upgrade autopep8
+
 ### Astro Vim setup (This one has plenty of Vim conf and I'm feeling lazy it up on my own)
 git clone https://github.com/kabinspace/AstroVim ~/.config/nvim
 nvim +PackerSync
 ln -s ~/.dotfiles/user ~/.config/nvim/lua/user
 zsh_source
-nvim -c ":LspInstall html cssls tsserver pyright prosemd_lsp"
+nvim -c ":LspInstall html cssls tsserver pyright prosemd_lsp eslint"
 nvim -c ":TSInstall html css javascript markdown tsx typescript"
 
 ### Git email username setup
--- user/init.lua
+++ user/init.lua
@@ -2,3 +2,15 @@ require('telescope').setup{  defaults = { file_ignore_patterns = { "node_modules
 require("luasnip").filetype_extend("javascript", { "html" })
 require("luasnip").filetype_extend("javascriptreact", { "html" })
 require("luasnip").filetype_extend("typescript", { "html" })
+local null_ls = require("null-ls")
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.autopep8
+    },
+    on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+          vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+        end
+      end,
+})
